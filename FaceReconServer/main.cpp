897ecfb66f1ace5ccd369a4af08a209fc80a1f2b
Nandrean David-Cristian <ndavid_cristian@yahoo.com>
2022-11-23 12:44:54 +0100

    base64 conversions to common_libraries

--- FaceReconServer/main.cpp
+++ FaceReconServer/main.cpp
@@ -1,3 +1,4 @@
+#include "utilities.h"
 #include <crow.h>
 #include <crow/utility.h>
 #include <filesystem>
@@ -12,88 +13,6 @@
 static size_t WriteCallback(void* contents, size_t size, size_t nmemb, void* userp) {
 	((std::string*)userp)->append((char*)contents, size * nmemb);
 	return size * nmemb;
-}
-
-// Check if it is a real base64 code
-static inline bool is_base64(unsigned char c)
-{
-	return (std::isalnum(c) || (c == '+') || (c == '/'));
-}
-
-std::string base64_decode(const std::string& encoded_string)
-{
-	int in_len = encoded_string.size();
-	int i = 0;
-	int j = 0;
-	int in_ = 0;
-	unsigned char char_array_4[4], char_array_3[3];
-	std::string ret;
-    const std::string base64_chars =
-		"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-		"abcdefghijklmnopqrstuvwxyz"
-		"0123456789+/";
-
-	while (in_len-- && (encoded_string[in_] != '=') && is_base64(encoded_string[in_]))
-	{
-		char_array_4[i++] = encoded_string[in_]; in_++;
-
-		if (i == 4)
-		{
-			for (i = 0; i < 4; i++)
-			{
-				char_array_4[i] = base64_chars.find(char_array_4[i]);
-			}
-
-			char_array_3[0] = (char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4);
-			char_array_3[1] = ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
-			char_array_3[2] = ((char_array_4[2] & 0x3) << 6) + char_array_4[3];
-
-			for (i = 0; (i < 3); i++)
-			{
-				ret += char_array_3[i];
-			}
-
-			i = 0;
-		}
-	}
-
-	if (i)
-	{
-		for (j = i; j < 4; j++)
-		{
-			char_array_4[j] = 0;
-		}
-
-		for (j = 0; j < 4; j++)
-		{
-			char_array_4[j] = base64_chars.find(char_array_4[j]);
-		}
-
-		char_array_3[0] = (char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4);
-		char_array_3[1] = ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
-		char_array_3[2] = ((char_array_4[2] & 0x3) << 6) + char_array_4[3];
-
-		for (j = 0; (j < i - 1); j++)
-		{
-			ret += char_array_3[j];
-		}
-	}
-
-	return ret;
-}
-
-
-cv::Mat str2mat(const std::string& s)
-{
-	// Decode data
-	std::string decoded_string = base64_decode(s);
-	std::vector<uchar> data(decoded_string.begin(), decoded_string.end());
-
-	cv::Mat img = cv::imdecode(data, cv::IMREAD_UNCHANGED);
-	// Check if the mat image is the same as base64
-	cv::imshow("Image", img);
-	cv::waitKey();
-	return img;
 }
 
 void getData() 
@@ -160,7 +79,7 @@ int main()
 		base64 = base64.substr(base64.find_first_of(",") + 1);
 		
 		// Encode from base64 to mat
-		cv::Mat imgMat = str2mat(base64);
+		cv::Mat imgMat = base64_utilities::str2mat(base64);
 
 		// Save the mat to the given folder
 		createFolderandMatFile(imgMat);
