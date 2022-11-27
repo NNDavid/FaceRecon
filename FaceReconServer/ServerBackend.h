b62b1be65d8048e01df31afd0c013a4f4f38bd4c
Nandrean David-Cristian <ndavid_cristian@yahoo.com>
2022-11-27 16:00:32 +0100

    Final program version

--- FaceReconServer/ServerBackend.h
+++ FaceReconServer/ServerBackend.h
@@ -18,12 +18,15 @@ public:
 	~ServerBackend();
 	void init();
 	void reg(const crow::request& req, crow::response& res);
+	void send(const crow::request& req, crow::response& res);
 	void enter(const crow::request& req, crow::response& res);
 private:
 	bool insertDatabaseData(const Data& data);
 	bool updateDatabaseData(const size_t id);
 	void updateLocalDatabase(const crow::json::rvalue& response);
 	bool getDatabaseData(crow::json::rvalue& response);
+	static bool emailCheck(const std::string& email);
+	bool checkEmailIsUnique(const std::string& email);
 	static inline std::string DataToString(const Data& data);
 	std::pair<size_t, double> compareImgWithDatabase(const cv::Mat& image);
 	CURL* curl;
